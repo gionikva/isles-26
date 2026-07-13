fixed model dimension mismatches and implemented loss calculation; started working on training code

model_testing.py
@@ -1,10 +1,13 @@
+from matplotlib import pyplot as plt
+import numpy as np
 import torch
-from models.models import GlobalAnchorDetector, GhostConv3D, SpatialAnchorFiLM
+from models.models import GlobalAnchorDetector, GhostConv3D, LightMedSeg, SpatialAnchorFiLM
 from models.lspm import LSPM
 from utils.dataset import ISLESDataset
+from monai.transforms import ResizeWithPadOrCrop
+import torch.nn.functional as F
 
-
-def main():
+def test_components():
     anchor_detector = GlobalAnchorDetector(1, 16, 8)
     ghost_conv = GhostConv3D(1, 8, downscale=False)
     lspm = LSPM()
@@ -26,6 +29,74 @@ def main():
     
     
     print(f0.shape)
+
+def visualize_prediction(model, image, mask):
+    
+    image = image.cpu()
+    mask = mask.cpu()
+    model = model.cpu()
+    prediction = F.softmax(model(image.unsqueeze(0)), dim=1)
+    
+    def show_slices(slices):
+        fig, axes = plt.subplots(1, len(slices))
+        for i, (mri, mask, prediction) in enumerate(slices):
+            axes[i].imshow(mri.T, cmap="gray", origin="lower")
+            axes[i].imshow(mask.T, cmap='autumn', alpha=0.3, interpolation='none')
+            axes[i].imshow(prediction.T, cmap='winter', alpha=0.3, interpolation='none')
+
+    print(image.shape)
+
+    image = image.squeeze(0).numpy()
+    mask = mask.squeeze(0).numpy()
+    prediction = prediction.squeeze(0).squeeze(0).detach().numpy()
+    mask = np.ma.masked_where(mask == 0, mask)
+    prediction = np.ma.masked_where(prediction < 0.5, prediction)
+
+    mri_0 = image[128, :, :]
+    mask_0 = mask[128, :, :]
+    prediction_0 = prediction[128, :, :]
+    
+    mri_1 = image[:, 128, :]
+    mask_1 = mask[:, 128, :]
+    prediction_1 = prediction[:, 128, :]
+
+    
+    mri_2 = image[:, :, 128]
+    mask_2 = mask[:, :, 128]
+    prediction_2 = prediction[:, :, 128]
+    
+   
+    show_slices([(mri_0, mask_0, prediction_0), (mri_1, mask_1, prediction_1), (mri_2, mask_2, prediction_2)])
+    plt.tight_layout()
+    plt.show()
+    
+    print(prediction.shape)
+
+def test_model():
+    dataset = ISLESDataset()
+
+    img = dataset[500]['image']
+    mask = dataset[500]['mask']
+    
+    
+    
+    print(img.shape)
+    print(mask.shape)
+    
+    standardize_grid = ResizeWithPadOrCrop(spatial_size=(256, 256, 256), mode='constant')
+    
+    # print (final_image.shape)
+    model = LightMedSeg(n_classes=2, num_anchors=8)
+    
+    prediction = model(img.unsqueeze(0))
+    print(prediction.shape)
+    
+    # visualize_prediction(model, img, mask)
+  
+
+def main():
+    # test_components()
+    test_model()
 
 
 if __name__ == "__main__":

models/lspm.py
@@ -74,7 +74,7 @@ class SpatialGatingHead(nn.Module):
         x2 = self.conv2(x1)
         alphas = self.projection(x2)
         
-        return alphas[:, 0, ...], alphas[:, 1, ...]
+        return alphas[:, 0:1, ...], alphas[:, 1:2, ...]
 
 class AdaptiveFeatureMixer(nn.Module):
     def __init__(self, in_channels=8):

train.py
@@ -1,11 +1,57 @@
+import torch
+from models.models import LightMedSeg
+from models.loss import LightMedSegLoss
 from utils.dataset import ISLESDataset
-import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+
+
+import torch.nn as nn
+from torch.nn import CrossEntropyLoss
+from model_testing import visualize_prediction
+
+import torch.nn.functional as F
+from tqdm import tqdm
+
+
+
 
 def main():
-    dataset = ISLESDataset()
-    
-    
-    
+    dataset = ISLESDataset(range=(0, 20))
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+    model = LightMedSeg(n_classes=2, num_anchors=8)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    model = model.to(device)
+
+    NUM_EPOCHS = 1
+
+    LR = 0.001
+
+    optimizer = AdamW(model.parameters(), lr=LR)
+
+    criterion = CrossEntropyLoss()
+
+    for EPOCH in range(NUM_EPOCHS):
+        for batch in tqdm(dataloader):
+            images = batch["image"].to(device)
+            masks = batch["mask"].to(device)
+
+            predictions = model(images)
+
+            print(masks[0])
+            # print(predictions.shape, masks.shape)
+
+            loss = criterion(predictions, masks)
+
+            loss.backward()
+            optimizer.step()
+
+    image = dataset[0]["image"].to(device)
+    mask = dataset[0]["mask"].to(device)
+
+    visualize_prediction(model, image, mask)
+
 
 if __name__ == "__main__":
     main()

utils/dataset.py
@@ -4,14 +4,19 @@ from PIL.Image import Image
 from torch.utils.data import Dataset
 import torch
 import nibabel as nib
+from monai.transforms import ResizeWithPadOrCrop
 from glob import glob
 from os import scandir, listdir
 import os
 
 
 class ISLESDataset(Dataset):
-    def __init__(self, split="train"):
+    # mask_add_bgc: whether to add a background channel to the target mask
+    def __init__(self, split="train", range=None, mask_add_bgc=True):
         super().__init__()
+        
+        self.mask_add_bgc = mask_add_bgc
+        
         self.metadata = []
         self.features = []
         self.labels = []
@@ -26,12 +31,25 @@ class ISLESDataset(Dataset):
             self.metadata.append(os.path.join(path, "meta.csv"))
             self.features.append(os.path.join(path, "img.nii.gz"))
             self.labels.append(os.path.join(path, "mask.nii.gz"))
+    
+        if range is not None:
+            self.metadata = self.metadata[range[0]:range[1]]
+            self.features = self.features[range[0]:range[1]]
+            self.labels = self.labels[range[0]:range[1]]
+        
+        self.standardize_grid = ResizeWithPadOrCrop(spatial_size=(256, 256, 256), mode='constant')
 
     def __getitem__(self, idx):
-        image = torch.tensor(nib.load(self.features[idx]).get_fdata())
-        mask = torch.tensor(nib.load(self.labels[idx]).get_fdata())
+        image = torch.tensor(nib.load(self.features[idx]).get_fdata(), dtype=torch.float)
+        mask = torch.tensor(nib.load(self.labels[idx]).get_fdata(), dtype=torch.float)
+        image = self.standardize_grid(image.unsqueeze(0))
+        mask = self.standardize_grid(mask.unsqueeze(0))
+        if self.mask_add_bgc:
+            bg = 1.0 - mask
+            mask = torch.cat([mask, bg], dim = 0)
         meta = pd.read_csv(self.metadata[idx])
-        return {"image": image, "mask": mask, "metadata": meta}
+        # return {"image": image, "mask": mask, "metadata": meta}
+        return {"image": image, "mask": mask}
 
     def __len__(self):
         return len(self.features)