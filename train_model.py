5cb0fc44b061e61d02443965077968f7ef230f6d
gionikva <gionikva.2014@gmail.com>
2026-07-27 15:57:47 -0700

    fixed typo

--- train_model.py
+++ train_model.py
@@ -322,7 +322,7 @@ def main():
 
     args = parse_arguments()
 
-    output_dir = args["output"]
+    output_dir = args["output_dir"]
     lr_range = args["lr_range"]
     epochs = args["epochs"]
     batch_size = args["batch_size"]
