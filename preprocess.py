f0759a38c355ae661c85f93bcf71be5f684e1769
gionikva <gionikva.2014@gmail.com>
2026-07-31 13:09:49 -0700

    updated preprocess to shuffle files

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,12 @@
 import os
-from os import scandir, listdir, mkdir
+from random import shuffle
 from shutil import copy
+from os import scandir, listdir, mkdir
 import SimpleITK as sitk
 import nibabel as nib
 import numpy as np
+from tqdm import tqdm
+
 
 def load_raw_fps():
     metadata = []
@@ -19,20 +22,21 @@ def load_raw_fps():
             path = os.path.join(subdir.path, "ses-1/anat")
             name = subdir.name
             metadata.append(os.path.join(path, f"{name}_ses-1_metadata.csv"))
-            
+
             mris.append(
                 os.path.join(path, f"{name}_ses-1_space-orig_desc-brain_T1w.nii.gz")
             )
-            
+
             masks.append(
                 os.path.join(
                     path,
                     f"{name}_ses-1_space-orig_label-lesion_desc-T1lesion_mask.nii.gz",
                 )
             )
-            
-    return (metadata, mris, masks)
-    
+
+    return list(zip(metadata, mris, masks))
+
+
 def n4_bias_field(array):
     arr = np.transpose(array)
     img = sitk.GetImageFromArray(arr)
@@ -42,52 +46,70 @@ def n4_bias_field(array):
     shrink_factor = 4
     shrunk = sitk.Shrink(img, [shrink_factor] * img.GetDimension())
     mask_shrunk = sitk.Shrink(mask, [shrink_factor] * img.GetDimension())
-    
+
     corrector.Execute(shrunk, mask_shrunk)
-    
+
     log_bias_field = corrector.GetLogBiasFieldAsImage(img)
     corrected_image = img / sitk.Exp(log_bias_field)
     # corrected_image = corrector.Execute(img, mask)
-    
+
     return np.transpose(sitk.GetArrayFromImage(corrected_image))
+
 
 def percentile_clip(array):
     lower = np.percentile(array, 1)
     upper = np.percentile(array, 99)
     return np.clip(array, lower, upper)
 
+
 def z_score(array):
     mean = np.mean(array)
     std = np.std(array)
     return (array - mean) / std
-    
+
+
 def process_image(path, out_path):
     orig_img = nib.load(path)
     arr = orig_img.get_fdata()
     affine = orig_img.affine
     header = orig_img.header
-    
+
     arr = n4_bias_field(arr)
     arr = percentile_clip(arr)
     arr = z_score(arr)
-    
+
     img = nib.Nifti1Image(arr, affine=affine, header=header)
     nib.save(img, out_path)
 
 
 def main():
-    mkdir('./data')
-    metadata, mris, masks = load_raw_fps()
-    print(len(metadata))
-    for i in range(len(metadata)):
-        mkdir(f'./data/{i}')
-        copy(metadata[i], f'./data/{i}/meta.csv')
-        copy(masks[i], f'./data/{i}/mask.nii.gz')
-        process_image(mris[i], f'./data/{i}/img.nii.gz')
-        print(f"Finished processing image {i}")
-        
+    files = load_raw_fps()
+    shuffle(files)
 
-    
+    # Ratio of training set images to total images
+    train_ratio = 0.9
+    n_train = int(len(files) * train_ratio)
+    n_test = len(files) - n_train
+
+    print(f"Total data points: {len(files)}")
+    for i in tqdm(range(n_train), desc="Train set"):
+        os.makedirs(f"./data/train/{i}", exist_ok=True)
+
+        meta, mask, mri = files[i]
+
+        copy(meta, f"./data/train/{i}/meta.csv")
+        copy(mask, f"./data/train/{i}/mask.nii.gz")
+        process_image(mri, f"./data/train/{i}/img.nii.gz")
+
+    for i in tqdm(range(n_test), desc="Test set"):
+        os.makedirs(f"./data/test/{i}", exist_ok=True)
+
+        meta, mask, mri = files[i + n_train]
+
+        copy(meta, f"./data/test/{i}/meta.csv")
+        copy(mask, f"./data/test/{i}/mask.nii.gz")
+        process_image(mri, f"./data/test/{i}/img.nii.gz")
+
 
 if __name__ == "__main__":
     main()
